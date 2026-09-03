5bfc115568e055d390c1bf6b84f816820ae5a97e
lwy0810 <lwy810@naver.com>
2026-09-04 01:33:25 +0900

    fix(battle): 두 번째 전투 진입 시 크래시 나던 널 리스트 버그 수정
     -_battleMemberList 미초기화로 두 번째 전투 진입 시 크래시하던 문제 수정. 매 전투 생성 시 Clear 후 재생성하도록 변경
    
    - Refs: Issue #48

--- Assets/Scripts/Battle/CreateCommandBattleMemberSystem.cs
+++ Assets/Scripts/Battle/CreateCommandBattleMemberSystem.cs
@@ -4,36 +4,34 @@ using UnityEngine;
 
 public class CreateCommandBattleMemberSystem : MonoBehaviour
 {
-    public List<GameObject> _battleMemberList;
-
+    // 예전에는 이 필드가 초기화되지 않아 CreateCommandBattleMember() 가 항상 null 을
+    // 반환했고, 배틀 씬을 두 번째로 들어갈 때 BattleManager.Battle() 에서
+    // NullReferenceException 이 발생했다.
+    public List<GameObject> _battleMemberList = new List<GameObject>();
 
 
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public List<GameObject> CreateCommandBattleMember( GameObject _battleMember, Transform _BattleMemberBarTransform)
+    public List<GameObject> CreateCommandBattleMember(GameObject _battleMember, Transform _BattleMemberBarTransform)
     {
+        _battleMemberList.Clear();
+
         for (int i = 0; i < GameManager.GameInstance._characterPrefabs.Length; i++)
         {
-            Instantiate(_battleMember, _BattleMemberBarTransform);
+            GameObject _member = Instantiate(_battleMember, _BattleMemberBarTransform);
+            _battleMemberList.Add(_member);
         }
 
         return _battleMemberList;
-
     }
-
-
-
-
-
-
 
 }

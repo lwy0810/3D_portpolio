bd17059f4ad1daac15087e327039fbd8687a60d7
lwy0810 <lwy810@naver.com>
2026-09-04 09:58:54 +0900

    refactor(ui): Member 내 속성 변화에 따른 배경 변화 기능 제거

--- Assets/Scripts/Field/Member.cs
+++ Assets/Scripts/Field/Member.cs
@@ -45,19 +45,16 @@ public class Member : MonoBehaviour, IPointerClickHandler
 
     public void _elementBackgroundSet()
     {
-        if (_characterComponent.Stat.Element == "fire")
-        {
-            _elementBackground.color = Color.red;
              
-        }
-        else if (_characterComponent.Stat.Element == "wind")
-        {
-            _elementBackground.color = Color.green;
-        }
-        else if (_characterComponent.Stat.Element == "water")
-        {
-            _elementBackground.color = Color.blue;
-        }
+        //}
+        //else if (_characterComponent.Stat.Element == "wind")
+        //{
+        //    _elementBackground.color = Color.green;
+        //}
+        //else if (_characterComponent.Stat.Element == "water")
+        //{
+        //    _elementBackground.color = Color.blue;
+        //}
 
         Color c = _elementBackground.color;
         c.a = 120 / 255f;
